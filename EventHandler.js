6dcb847a122eb4a471543fa573ab6bd6c806e4f0
Yehonal <hw.2@hotmail.it>
2015-05-08 15:55:42 +0200

    various fixes and changes

--- EventHandler.js
+++ EventHandler.js
@@ -11,6 +11,8 @@ hw2.define([
                 a: ["private", "static"], n: "instances", v: new Array()
             },
             {
+                // Array object is very fexible, it can contains both associative values and numerical
+                // we've just to use specific add/delete/loop operations
                 a: "private", n: "triggers", v: new Array()
             },
             /**
@@ -27,24 +29,39 @@ hw2.define([
                 }
             },
             {
-                a: "public", n: "bind", v: function (obj) {
-                    if (!this._i.triggers.indexOf(obj))
-                        this._i.triggers.push(obj);
+                a: "public", n: "bind", v: function (obj, id, force) {
+                    if (force || (id && !this._i.triggers[id] || this._i.triggers.indexOf(obj) < 0)) {
+                        // if id is defined, then use this as array key
+                        id && (this._i.triggers[id] = obj) || this._i.triggers.push(obj);
+                    }
                 }
             },
             {
-                a: "public", n: "unbind", v: function (obj) {
-                    $.Array.remove(this._i.triggers, obj);
+                a: "public", n: "unbind", v: function (objOrId) {
+                    var obj = null;
+                    if (typeof objOrId == "string") {
+                        obj = this._i.triggers[objOrId];
+                        delete this._i.triggers[objOrId];
+                    } else {
+                        var index = this._i.triggers.indexOf(objOrId);
+                        obj = this._i.triggers[index];
+                        this._i.triggers.splice(index, 1);
+                    }
+
+                    return obj; // return the removed object
                 }
             },
             {
                 a: "public", n: "trigger", v: function (func /*, arguments */) {
+                    var that = this;
                     var res = [];
-                    for (var index = 0; index < this._i.triggers.length; ++index) {
-                        var f = this._i.triggers[index][func];
+                    var args = arguments;
+
+                    Object.keys(this._i.triggers).map(function (key) {
+                        var f = that._i.triggers[key][func];
                         if (typeof f === 'function')
-                            res.push(f.apply(this._i.triggers[index], Array.prototype.slice.call(arguments, 1)));
-                    }
+                            res.push(f.apply(that._i.triggers[key], Array.prototype.slice.call(args, 1)));
+                    });
 
                     return $.Async.all(res);
                 }
